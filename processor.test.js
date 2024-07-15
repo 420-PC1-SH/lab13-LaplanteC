6a4d7d603dbf30dfeda0d208ad489f0e3f0cd377
LaplanteC <laplantec.msi@gmail.com>
2024-07-15 09:54:20 -0400

    exercice 1 requis 2 fail

--- processor.test.js
+++ processor.test.js
@@ -42,4 +42,9 @@ describe("transmission processor", function () {
         });
     });
 
+    test("converts id to a number", function () {
+        const expectedError = new Error('id cannot be converted to number');
+        expect(() => { processor("idInStringFormat::<932829840830053761>"); }).toThrow(expectedError);
+    });
+
 });
