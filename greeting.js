af9cd305f4df77307645983a08dcf15c3e830443
LaplanteC <laplantec.msi@gmail.com>
2024-07-15 12:25:07 -0400

    exercice 2 requis 4 pass

--- greeting.js
+++ greeting.js
@@ -1,4 +1,10 @@
 function greet(name) {
+    if (Array.isArray(name)) {
+        let length = name.length;
+        if (length === 2) {
+            return "Hello, " + name[0] + " and " + name[1];
+        }
+    }
     if (name === undefined || name === null || name === "") {
         name = "there";
     }
