714e8c2d073cbe87b272c6d060f643d4d3471fc3
LaplanteC <laplantec.msi@gmail.com>
2024-07-15 11:34:16 -0400

    exercice 2 requis 2 fail (undefined)

--- greeting.test.js
+++ greeting.test.js
@@ -8,4 +8,11 @@ describe("greeting with name", function() {
         expect(result).toEqual("Hello, Luke");
     });
 
+    describe("returns «Hello, there» when name is undefined, null or empty", function() {
+        test("when name is undefined, returns «Hello, there»", function() {
+            let name
+            let result = greet(name);
+            expect(result).toEqual("Hello, there");
+        });
+    });
 });
