4749c2b196063aef836eb0d2a575291eef9849e0
LaplanteC <laplantec.msi@gmail.com>
2024-07-15 15:33:27 -0400

    exercice 2 requis 5 pass

--- greeting.js
+++ greeting.js
@@ -4,6 +4,13 @@ function greet(name) {
         if (length === 2) {
             return "Hello, " + name[0] + " and " + name[1];
         }
+        else {
+            let greeting = "Hello, ";
+            for (let i = 0; i < length-1; i++) {
+                greeting = greeting + name[i] + ", ";
+            }
+            return greeting + "and " + name[length-1];
+        }
     }
     if (name === undefined || name === null || name === "") {
         name = "there";
